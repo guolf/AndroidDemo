7200e7cee48dfbd60c15c1a586b686010d2a3f27
guo <guo@guolingfa.cn>
2015-04-27 20:01:32 +0800

    Initial commit

---
+++ app/src/main/java/cn/guolf/androiddemo/ui/EditTextDelActivity.java
@@ -0,0 +1,19 @@
+package cn.guolf.androiddemo.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.guolf.androiddemo.R;
+
+/**
+ * Created by glf on 2015/4/27.
+ */
+public class EditTextDelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_del_edit);
+    }
+}
